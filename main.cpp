2bc2271182009a699d36a8d671b8f4122261bc03
Victor Ignatenkov <vetekm@yahoo.com>
2016-03-13 13:54:00 -0700

    Create a board, fill it up and print it out

--- main.cpp
+++ main.cpp
@@ -11,8 +11,22 @@ using namespace std;
 #define DRAW 0
 #define NOT_OVER -1
 
-int b[3][3];
-int MAXDEPTH = 9;
+/* BOARD */
+#define BRD_LENGTH  7
+#define C_T 1
+#define C_W 2
+#define C_D 3
+#define C_X 4
+
+#define H_T 5
+#define H_W 6
+#define H_D 7
+#define H_X 8
+
+int board[7][7];
+char labels[] = {'-', 'C', '~', '*', 'X', 't', '+', '@', 'x'};
+
+int MAXDEPTH = 49;
 
 
 void makeMove();
@@ -22,33 +36,63 @@ int evaluate();
 int checkForWinner();
 void checkGameOver();
 void getAMove();
-void setup();
+void setupBoard();
 void printBoard();
 
 int main() {
-    setup();
+    setupBoard();
     printBoard();
-    for(;;){
-        getAMove();
-        checkGameOver();
-        makeMove();
-        checkGameOver();
-    }
+//    for(;;){
+////        getAMove();
+////        checkGameOver();
+////        makeMove();
+////        checkGameOver();
+//    }
 }
 
-void setup(){
-    for (int i = 0; i < 3; i++){
-        for (int j = 0; j < 3; j++){
-            b[i][j] = EMPTY;
+void setupBoard(){
+    for (int i = 0; i < BRD_LENGTH; i++){
+        for (int j = 0; j < BRD_LENGTH; j++){
+            /* Computer half */
+            if(i == 0 && (j == 1 || j == 2 || j == 4 || j == 5 )){
+                board[i][j] = C_T;
+            }
+            else if(i == 1 && (j == 2 || j == 4)){
+                board[i][j] = C_W;
+            }
+            else if(i == 1 && j == 3){
+                board[i][j] = C_D;
+            }
+            else if(i == 2 && (j == 0 || j == 1 || j == 5 || j == 6)){
+                board[i][j] = C_X;
+            }
+
+            /* Human Half */
+            else if(i == 6 && ( j==1 ||  j == 2 || j == 4 || j == 5 )){
+                board[i][j] = H_T;
+            }
+            else if(i == 5 && (j == 2 || j == 4)){
+                board[i][j] = H_W;
+            }
+            else if(i == 5 && j == 3){
+                board[i][j] = H_D;
+            }
+            else if(i == 4 && (j == 0 || j == 1 || j == 5 || j == 6)){
+                board[i][j] = H_X;
+            } else
+            board[i][j] = EMPTY;
         }
     }
 }
 
 void printBoard(){
     cout << endl;
-    cout << b[0][0] << " " << b[0][1] << " " << b[0][2] << endl;
-    cout << b[1][0] << " " << b[1][1] << " " << b[1][2] << endl;
-    cout << b[2][0] << " " << b[2][1] << " " << b[2][2] << endl;
+    for(int i = 0; i < BRD_LENGTH; i++){
+        for (int j = 0; j < BRD_LENGTH; j++){
+           cout << labels[board[i][j]] << " ";
+        }
+        cout << endl;
+    }
 }
 
 void getAMove(){
@@ -56,7 +100,7 @@ void getAMove(){
     int j;
     cout << "Enter your move: " ;
     cin >> i >> j;
-    b[i][j] = H;
+    board[i][j] = H;
 }
 
 void checkGameOver(){
@@ -79,20 +123,20 @@ void makeMove(){
     int best =-20000, depth = 1, score, mi, mj;
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            if(b[i][j] == EMPTY){
-                b[i][j] = C; //make move board
+            if(board[i][j] == EMPTY){
+                board[i][j] = C; //make move board
                 score = min(depth+1);
                 if(score > best){
                     mi = i;
                     mj = j;
                     best = score;
                 }
-                b[i][j] = EMPTY; //undo move
+                board[i][j] = EMPTY; //undo move
             }
         }
     }
     cout << "my move is" << mi << " " << mj << endl;
-    b[mi][mj] = C;
+    board[mi][mj] = C;
 }
 
 int min(int depth){
@@ -102,11 +146,11 @@ int min(int depth){
     if(depth == MAXDEPTH) return (evaluate());
     for(int i = 0; i < 3; i++){
         for(int j = 0; j < 3; j++){
-            if(b[i][j] == EMPTY){
-                b[i][j] = H; //make move on board
+            if(board[i][j] == EMPTY){
+                board[i][j] = H; //make move on board
                 score = max(depth+1);
                 if(score < best) best = score;
-                b[i][j] = EMPTY; // undo move
+                board[i][j] = EMPTY; // undo move
             }
         }
     }
@@ -120,11 +164,11 @@ int max(int depth){
     if(depth == MAXDEPTH) return (evaluate());
     for(int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            if(b[i][j] == EMPTY){
-                b[i][j] = C; //make move on board
+            if(board[i][j] == EMPTY){
+                board[i][j] = C; //make move on board
                 score = min(depth + 1);
                 if(score > best) best = score;
-                b[i][j] = EMPTY; //undo move
+                board[i][j] = EMPTY; //undo move
             }
         }
     }
@@ -132,28 +176,28 @@ int max(int depth){
 }
 
 int checkForWinner(){
-    if ((b[0][0] == C ) && (b[0][1] == C ) && (b[0][2] == C )
-        || (b[1][0] == C ) && (b[1][1] == C ) && (b[1][2] == C )
-        || (b[2][0] == C ) && (b[2][1] == C ) && (b[2][2] == C )
-        || (b[0][0] == C ) && (b[1][0] == C ) && (b[2][0] == C )
-        || (b[0][1] == C ) && (b[1][1] == C ) && (b[2][1] == C )
-        || (b[0][2] == C ) && (b[1][2] == C ) && (b[2][2] == C )
-        || (b[0][0] == C ) && (b[1][1] == C ) && (b[2][2] == C )
-        || (b[0][2] == C ) && (b[1][1] == C ) && (b[2][0] == C ))
+    if ((board[0][0] == C ) && (board[0][1] == C ) && (board[0][2] == C )
+        || (board[1][0] == C ) && (board[1][1] == C ) && (board[1][2] == C )
+        || (board[2][0] == C ) && (board[2][1] == C ) && (board[2][2] == C )
+        || (board[0][0] == C ) && (board[1][0] == C ) && (board[2][0] == C )
+        || (board[0][1] == C ) && (board[1][1] == C ) && (board[2][1] == C )
+        || (board[0][2] == C ) && (board[1][2] == C ) && (board[2][2] == C )
+        || (board[0][0] == C ) && (board[1][1] == C ) && (board[2][2] == C )
+        || (board[0][2] == C ) && (board[1][1] == C ) && (board[2][0] == C ))
         return C_WINS; //computer wins 5000
-    if ((b[0][0] == H ) && (b[0][1] == H ) && (b[0][2] == H )
-        || (b[1][0] == H ) && (b[1][1] == H ) && (b[1][2] == H )
-        || (b[2][0] == H ) && (b[2][1] == H ) && (b[2][2] == H )
-        || (b[0][0] == H ) && (b[1][0] == H ) && (b[2][0] == H )
-        || (b[0][1] == H ) && (b[1][1] == H ) && (b[2][1] == H )
-        || (b[0][2] == H ) && (b[1][2] == H ) && (b[2][2] == H )
-        || (b[0][0] == H ) && (b[1][1] == H ) && (b[2][2] == H )
-        || (b[0][2] == H ) && (b[1][1] == H ) && (b[2][0] == H ))
+    if ((board[0][0] == H ) && (board[0][1] == H ) && (board[0][2] == H )
+        || (board[1][0] == H ) && (board[1][1] == H ) && (board[1][2] == H )
+        || (board[2][0] == H ) && (board[2][1] == H ) && (board[2][2] == H )
+        || (board[0][0] == H ) && (board[1][0] == H ) && (board[2][0] == H )
+        || (board[0][1] == H ) && (board[1][1] == H ) && (board[2][1] == H )
+        || (board[0][2] == H ) && (board[1][2] == H ) && (board[2][2] == H )
+        || (board[0][0] == H ) && (board[1][1] == H ) && (board[2][2] == H )
+        || (board[0][2] == H ) && (board[1][1] == H ) && (board[2][0] == H ))
         return H_WINS; //human wins -5000
 
     for(int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            if(b[i][j] == EMPTY) return NOT_OVER; //game not over
+            if(board[i][j] == EMPTY) return NOT_OVER; //game not over
         }
     }
     return DRAW; //game over - draw 0
